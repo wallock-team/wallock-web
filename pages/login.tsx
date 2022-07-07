f45a342ba1161c116c635b5bb7c5a7d7ed278b2f
Quang Pham <quangpham.245@protonmail.com>
2022-07-07 16:40:28 +0700

    Install MUI

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,18 @@
-import { NextPage } from "next";
+import {
+  NextPage
+} from "next";
+
+import {
+  Button, Container, Typography
+} from "@mui/material";
 
 const Login: NextPage = () =>  {
-  return <h1>Hello! You are in Login page!</h1>
+  return (
+    <Container>
+      <Typography>Hello! You are in Login page!</Typography>
+      <Button>Click me!</Button>
+    </Container>
+  );
 }
 
 export default Login
