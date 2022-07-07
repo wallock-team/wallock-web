e4da15fff02101e72a7f45db97e8b58f655c052f
Quang Pham <quangpham.245@protonmail.com>
2022-07-07 18:06:09 +0700

    In pages/login.tsx: center logo

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,36 @@
-import {
-  NextPage
-} from "next";
+import { ReactNode } from "react";
+import { NextPage } from "next";
+import { Button } from "@mui/material";
+import GoogleIcon from '@mui/icons-material/Google';
 
-import {
-  Button, Typography
-} from "@mui/material";
+import Logo from "../components/logo";
 
 const Login: NextPage = () =>  {
   return <>
-      <Typography>Hello! You are in Login page!</Typography>
-      <Button>Click me!</Button>
+    
+    <CenterPositioned>
+      <Logo />
+    </CenterPositioned>
+
+    <Button
+      fullWidth
+      variant="contained"
+      startIcon={<GoogleIcon />}
+    >
+      Log in with Google
+    </Button>
   </>
+}
+
+const CenterPositioned = ({ children }: { children: ReactNode }) => {
+  return <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+    }}
+  >
+    {children}
+  </div>;
 }
 
 export default Login
