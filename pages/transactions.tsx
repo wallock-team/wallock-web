acd5472fe05b719865eaf990e0c8487ad28e02ef
Quang Pham <quangpham.245@protonmail.com>
2022-08-25 00:11:20 +0700

    Add tabs in transactions page

--- pages/transactions.tsx
+++ pages/transactions.tsx
@@ -1,10 +1,17 @@
-import { Container } from '@mui/material'
+import { Container, Tab, Tabs } from '@mui/material'
 import { NextPage } from 'next'
 import WalletBalance from '../components/wallet-balance'
 
 const Transactions: NextPage = () => (
   <Container>
     <WalletBalance balance={10_000_000} />
+    <Tabs variant='scrollable'>
+      <Tab label='08.01 - 08.07' />
+      <Tab label='08.08 - 08.14' />
+      <Tab label='Last week' />
+      <Tab label='This week' />
+      <Tab label='Next week' />
+    </Tabs>
   </Container>
 )
 
