a4afae197fb6e7bfe5e0331acb771c0f53b3a6e8
Hien Pham <pmhien2703@gmail.com>
2022-09-15 20:39:48 +0700

    Add LocalizationProvider Component for MUI TimePicker available

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { Container, CssBaseline } from '@mui/material'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { LocalizationProvider } from '@mui/x-date-pickers';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <CssBaseline />
-      <Component {...pageProps} />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </LocalizationProvider>
     </>
   )
 }
