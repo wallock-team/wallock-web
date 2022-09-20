3c616f7b76b50d059b740bd2b4c02e8f9c13bd8a
Hien Pham <pmhien2703@gmail.com>
2022-09-20 21:43:48 +0700

    Create Class API

---
+++ pages/api/api.ts
@@ -0,0 +1,28 @@
+import axios from '../../lib/auth/config-axios';
+import { transactionsApi } from './transactions-api2';
+
+export default class Api {
+    public static async fromServer(context: any) {
+        return new Api(context)
+    }
+
+    public static async fromWeb() {
+        return new Api()
+    }
+
+    public constructor(private readonly context?: any) {
+        let defaultsOption = ({
+            withCredentials: true,
+        })
+        let instance = axios.create(defaultsOption)
+
+        if (context) {
+            instance.defaults.headers.get.Cookie = String(context.req.headers.cookie)
+        }
+        this.transactions = transactionsApi(instance)
+    }
+
+    public readonly transactions: any
+    public readonly categories: any
+
+}
