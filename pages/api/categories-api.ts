3960b1dfc76d64214900fed340ff157a9ef804a7
Quang Pham <quangpham.245@protonmail.com>
2022-09-21 00:06:59 +0700

    Add CategoriesApi class

---
+++ pages/api/categories-api.ts
@@ -0,0 +1,13 @@
+import { Axios } from 'axios'
+
+export default class CategoriesApi {
+  public constructor(private readonly axios: Axios) {}
+
+  public async create() {}
+
+  public async update() {}
+
+  public async delete() {}
+
+  public async get() {}
+}
