49574ba0489e855653205696052d7acfecf79d93
Hien Pham <pmhien2703@gmail.com>
2022-09-24 18:26:22 +0700

    Change TransactionApis from arrow function to be class

--- pages/api/transactions-api.ts
+++ pages/api/transactions-api.ts
@@ -1,11 +1,13 @@
-export const transactionsApi = (configedAxios) => ({
-    configedAxios,
+import { Axios } from 'axios';
+export default class TransactionsApi {
+  public constructor(private readonly configedAxios: Axios) {}
+
     async getCurrentMonth() {
-        return await this.configedAxios.get('/transactions/current')
-    },
+        return await this.configedAxios.get('/transactions/current-month')
+    }
     async getById(id: any) {
         return await this.configedAxios.get(`/transactions/${id}`)
-    },
+    }
     initialTransition () {
         return { 
             amount: 0,
@@ -13,14 +15,14 @@ export const transactionsApi = (configedAxios) => ({
             note: '',
             cateId: 1,
         }
-    },
+    }
     async add (transaction: any) {
         return this.configedAxios.post(`/transactions`, transaction);
-    },
+    }
     async update(transaction: any) {
         return await this.configedAxios.patch(`/transactions`, transaction);
-    },
+    }
     async remove(id: number) {
         return await this.configedAxios.delete(`/transactions/${id}`);
     }
-})
+}
