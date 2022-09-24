bfa696ea1418713bd76e0ff3b8465dfd8e6f9621
Hien Pham <pmhien2703@gmail.com>
2022-09-24 18:27:46 +0700

    Implement CategoriesApis functions

--- pages/api/categories-api.ts
+++ pages/api/categories-api.ts
@@ -3,11 +3,19 @@ import { Axios } from 'axios'
 export default class CategoriesApi {
   public constructor(private readonly axios: Axios) {}
 
-  public async create() {}
+  public async create(createCategoryDto: any) {
+    return await this.axios.post('/categories', createCategoryDto)
+  }
 
-  public async update() {}
+  public async update(updateCategoryDto: any) {
+    return await this.axios.patch('/categories', updateCategoryDto)
+  }
 
-  public async delete() {}
+  public async delete(id: number) {
+    return await this.axios.delete(`/categories/${id}`);
+  }
 
-  public async get() {}
+  public async getById(id: number) {
+    return await this.axios.get(`/categories/${id}`);
+  }
 }
