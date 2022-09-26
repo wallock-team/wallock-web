479a6bdc2c587e5c49316263a8d7bf84c891eaea
Hien Pham <pmhien2703@gmail.com>
2022-09-26 19:49:46 +0700

    Fix typo (configedAxios -> configuredAxios)

--- pages/api/transactions-api.ts
+++ pages/api/transactions-api.ts
@@ -1,12 +1,12 @@
 import { Axios } from 'axios';
 export default class TransactionsApi {
-  public constructor(private readonly configedAxios: Axios) {}
+  public constructor(private readonly configuredAxios: Axios) {}
 
     async getCurrentMonth() {
-        return await this.configedAxios.get('/transactions/current-month')
+        return await this.configuredAxios.get('/transactions/current-month')
     }
     async getById(id: any) {
-        return await this.configedAxios.get(`/transactions/${id}`)
+        return await this.configuredAxios.get(`/transactions/${id}`)
     }
     initialTransition () {
         return { 
@@ -17,12 +17,13 @@ export default class TransactionsApi {
         }
     }
     async add (transaction: any) {
-        return this.configedAxios.post(`/transactions`, transaction);
+        console.log('Transaction added' + JSON.stringify(transaction))
+        return await this.configuredAxios.post('/transactions', transaction);
     }
     async update(transaction: any) {
-        return await this.configedAxios.patch(`/transactions`, transaction);
+        return await this.configuredAxios.patch(`/transactions`, transaction);
     }
     async remove(id: number) {
-        return await this.configedAxios.delete(`/transactions/${id}`);
+        return await this.configuredAxios.delete(`/transactions/${id}`);
     }
 }
