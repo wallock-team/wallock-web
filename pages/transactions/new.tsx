adf9eb1549cdefa7417d761ba8e6371fb053f43a
Quang Pham <quangpham.245@protonmail.com>
2022-09-28 01:08:41 +0700

    Lint new transaction page

--- pages/transactions/new.tsx
+++ pages/transactions/new.tsx
@@ -1,39 +1,58 @@
-import { Close } from "@mui/icons-material"
-import { AppBar, Button, Grid, Typography } from "@mui/material"
-import { NextPage } from "next"
-import { useState } from "react"
-import { useNavigate } from 'react-router-dom';
-import { useRouter } from "next/router"
-import TransactionForm from "../../components/transaction/transactionForm"
+import { Close } from '@mui/icons-material'
+import { AppBar, Button, Grid, Typography } from '@mui/material'
+import { NextPage } from 'next'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useRouter } from 'next/router'
+import TransactionForm from '../../components/transaction/transactionForm'
 import Api from '../../lib/api/api'
 
 const NewTransaction: NextPage = () => {
   let api = Api.fromWeb()
-  const [transaction, setTransaction] = useState(api.transactions.initialTransition())
+  const [transaction, setTransaction] = useState(
+    api.transactions.initialTransition()
+  )
   const router = useRouter()
   const handleAddTransaction = async () => {
-    await api.transactions.add({...transaction, amount: parseInt(transaction.amount)})
+    await api.transactions.add({
+      ...transaction,
+      amount: parseInt(transaction.amount),
+    })
     router.push('/transactions')
   }
-  console.log(transaction);
-  return (<>
-    <AppBar position='sticky' sx={{ px: 2 }}>
-      <Grid container justifyContent='space-between' sx={{ my: 2 }}>
-        <Grid container alignContent={'center'} item xs='auto'>
-          <Close sx={{ fontSize: 35, margin: 'auto' }}></Close>
-          <Typography sx={{ fontSize: 20, margin: 'auto' }}>Transaction</Typography>
+  console.log(transaction)
+  return (
+    <>
+      <AppBar position='sticky' sx={{ px: 2 }}>
+        <Grid container justifyContent='space-between' sx={{ my: 2 }}>
+          <Grid container alignContent={'center'} item xs='auto'>
+            <Close sx={{ fontSize: 35, margin: 'auto' }}></Close>
+            <Typography sx={{ fontSize: 20, margin: 'auto' }}>
+              Transaction
+            </Typography>
+          </Grid>
+          <Grid>
+            <Button sx={{ marginLeft: '10px' }} color='success'>
+              {' '}
+              Edit{' '}
+            </Button>
+            <Button
+              onClick={() => handleAddTransaction()}
+              sx={{ marginLeft: '10px' }}
+              color='error'
+            >
+              {' '}
+              Add{' '}
+            </Button>
+          </Grid>
         </Grid>
-        <Grid >
-          <Button sx={{ marginLeft: '10px' }} color="success"> Edit </Button>
-          <Button onClick={() => handleAddTransaction()} sx={{ marginLeft: '10px' }} color="error"> Add  </Button>
-        </Grid>
-      </Grid>
-    </AppBar>
-    <TransactionForm
-      transaction={transaction}
-      setTransaction={setTransaction}
-    />
-  </>)
+      </AppBar>
+      <TransactionForm
+        transaction={transaction}
+        setTransaction={setTransaction}
+      />
+    </>
+  )
 }
 
 export default NewTransaction
