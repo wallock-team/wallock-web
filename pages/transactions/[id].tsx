3e4de214e79bcabe6ec1361e99d158fb8d8ba6c7
Quang Pham <quangpham.245@protonmail.com>
2022-10-02 00:53:33 +0700

    Redesign transaction detail page

--- pages/transactions/[id].tsx
+++ pages/transactions/[id].tsx
@@ -1,50 +1,50 @@
-import { AppBar, Container, Grid, Typography, Button } from '@mui/material'
+import {
+  AppBar,
+  Container,
+  Typography,
+  Button,
+  Toolbar,
+  IconButton,
+} from '@mui/material'
 import { GetServerSideProps, NextPage } from 'next'
-import { Close } from '@mui/icons-material';
-import Link from 'next/link';
-import TransactionDetail from '../../components/transaction/transaction-detail';
-import withAuthPage from '../../lib/auth/withAuthPage';
-import Api from '../../lib/api/api';
+import { Close } from '@mui/icons-material'
+import withAuthPage from 'lib/auth/withAuthPage'
+import Api from 'lib/api/api'
+import { useRouter } from 'next/router'
+import EditIcon from '@mui/icons-material/Edit'
 
-export const getServerSideProps: GetServerSideProps = withAuthPage(async (context) => {
-  let apiServer = await Api.fromServer(context)
-  console.log(context)
-  let id = context.params.id
-  let response = await apiServer.transactions.getById(id)
-  return {
-    props: {
-     transaction: response.data,
-     id: id
+export const getServerSideProps: GetServerSideProps = withAuthPage(
+  async (context) => {
+    const api = await Api.fromServer(context)
+
+    const transaction = await api.transactions.getById(context.params?.id)
+    console.log(transaction)
+
+    return {
+      props: {
+        transaction,
+      },
     }
   }
-})
-const index: NextPage = (props) => {
-  
+)
+const TransactionDetail: NextPage = (props) => {
+  const router = useRouter()
   return (
     <>
       <AppBar position='sticky' sx={{ px: 2 }}>
-        <Grid container justifyContent='space-between' sx={{ my: 2 }}>
-          <Grid container alignContent={'center'} item xs='auto'>
-            <Close sx={{ fontSize: 35, margin: 'auto' }}></Close>
-            <Typography sx={{ fontSize: 20, margin: 'auto' }}>Transaction Detail</Typography>
-          </Grid>
-          <Grid >
-            <Link
-              href={{
-                pathname: 'edit/' + props.id,
-              }}
-            >
-              <a>Edit</a>
-            </Link>
-            <Button onClick={() => remove(props.id)} sx={{ marginLeft: '10px' }} color="error"> Delete  </Button>
-          </Grid>
-        </Grid>
+        <Toolbar>
+          <IconButton onClick={() => router.push('/transactions')}>
+            <Close />
+          </IconButton>
+          <Typography sx={{ flexGrow: 1 }}>Transaction detail</Typography>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </Toolbar>
       </AppBar>
-      <Container>
-        <TransactionDetail transaction={props.transaction}/>
-      </Container>
+      <Container></Container>
     </>
   )
 }
 
-export default index
+export default TransactionDetail
